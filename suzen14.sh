23e99e3bdadaaff3d96749781b4961b8b399ebcd
Fishman2410 <fishman241098@gmail.com>
2019-05-31 20:38:22 +0300

    Update suzen14.sh

--- suzen14.sh
+++ suzen14.sh
@@ -3,14 +3,6 @@ diary.txt  part1
 /home/suzen # cd part1
 /home/suzen/part1 # ls
 d2FodmFoM
-/home/suzen/part1 # cd d2FodmFoM
-/home/suzen/part1/d2FodmFoM # ls
-/home/suzen/part1/d2FodmFoM # cd -
-/home/suzen/part1
-/home/suzen/part1 # ls
-d2FodmFoM
-/home/suzen/part1 # cd home
-sh: cd: can't cd to home: No such file or directory
 /home/suzen/part1 # cd /home
 /home # ls
 john   suzen
@@ -23,13 +15,7 @@ part2
 /home/john/Documents # cd part2
 /home/john/Documents/part2 # ls
 WFlV2FpYm
-/home/john/Documents/part2 # cd /home
-/home # ls
-john   suzen
-/home # cd john
-/home/john # ls
-Desktop    Documents
-/home/john # cd Desktop
+/home/john # cd /home/john/Desktop
 /home/john/Desktop # ls
 part3
 /home/john/Desktop # cd part3
