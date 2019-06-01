8ac2b7470506b086c82394422005b08ed2d8c2a8
Fishman2410 <fishman241098@gmail.com>
2019-06-01 15:14:47 +0300

    Update suzen19.sh

--- suzen19.sh
+++ suzen19.sh
@@ -1,3 +1,3 @@
-/home/suzen # mkdir -p Documents/projects/lab19/using/bash/commands
-/home/suzen # cd Documents/projects/lab19/using/bash/commands
+mkdir -p Documents/projects/lab19/using/simple/bash/commands
+cd Documents/projects/lab19/using/simple/bash/commands
 ls
